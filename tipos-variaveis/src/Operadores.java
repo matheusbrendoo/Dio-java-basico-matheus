5d0e28a6c11c55b58bc404b29808a386da8c5495
matheusbrendoo <matheus.sousa09@hotmail.com>
2024-05-13 22:18:52 -0300

    push new codes

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,56 @@
+public class Operadores {
+
+    public static void main(String[] argis) {
+
+       /* int a, b;
+        a = 5;
+        b = 6;
+       
+       String resultado = "";
+        if(a==b)
+          resultado = "verdadeiro";
+          else
+          resultado = "falso"; 
+          ou faz mais assim: */
+
+        //*String resultado = a==b ?"verdadeiro" : "falso";
+
+        //* System.out.println(resultado); 
+        /*String nomeUm = "Matheus";
+        String nomeDois = "Brendo";
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean SimNao = numero1 == numero2;
+
+        System.out.println("NumeroUm é igual a NumeroDois? " + SimNao);
+
+        SimNao = numero1 != numero2;
+
+        System.out.println("NumeroUm é diferente a NumeoDois? " + SimNao);
+
+        if(numero1 > numero2){
+            System.out.println("Numero 1 é menor mesmo");*/
+
+        boolean condicao1=true;
+        boolean condicao2=false;
+
+        if(condicao1 || condicao2){
+                System.out.println("as duas condições são verdadeiras");
+
+        }
+
+        System.out.println("Fim");
+        
+        }
+
+        
+
+
+    }
